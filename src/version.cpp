8ea99dc137d393a736e7cf7109d2858096ec7907
RebornPowerHorse <leo@cleptrum.com>
2024-05-12 01:55:56 +0800

    fix boost missing requirement in CMakeList.txt

---
+++ src/version.cpp
@@ -0,0 +1,9 @@
+#include "version.h"
+
+using namespace wotreplay;
+
+const std::string Version::GIT_SHA1 = "5601e9c640c00e67a8239e944ad2a91178d983c4-dirty";
+const std::string Version::GIT_DATE = "Thu Feb 24 22:23:23 2022";
+const std::string Version::GIT_COMMIT_SUBJECT = "replace google link with github";
+const std::string Version::BUILD_DATE = "Sat May 11 17:01:38 2024";
+const std::string Version::BUILD_VERSION = "20220224.222323";
